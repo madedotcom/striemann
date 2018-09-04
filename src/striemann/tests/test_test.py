803a579b4da928af8f50d2244e7c1c1a42fb502e
John Lee <john.lee@made.com>
2018-09-04 17:52:41 +0100

    Fix expects matcher to match recent API change

---
+++ src/striemann/tests/test_test.py
@@ -0,0 +1,11 @@
+from expects import expect
+from striemann.test import fakes
+from striemann.test.expectsmatcher import contain_metric
+
+
+class TestExpectsMatcher:
+    def test(self):
+        metrics = fakes.FakeMetrics()
+        metrics.incrementCounter('Burgers sold')
+        metrics.recordGauge('Hunger level', 10)
+        expect(metrics).to(contain_metric('Hunger level'))
