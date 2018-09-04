e85d7b663cb5bc1cc67897173ed2fe44d5524e08
John Lee <john.lee@made.com>
2018-09-04 18:28:10 +0100

    black

--- src/striemann/tests/test_test.py
+++ src/striemann/tests/test_test.py
@@ -6,6 +6,6 @@ from striemann.test.expectsmatcher import contain_metric
 class TestExpectsMatcher:
     def test(self):
         metrics = fakes.FakeMetrics()
-        metrics.incrementCounter('Burgers sold')
-        metrics.recordGauge('Hunger level', 10)
-        expect(metrics).to(contain_metric('Hunger level'))
+        metrics.incrementCounter("Burgers sold")
+        metrics.recordGauge("Hunger level", 10)
+        expect(metrics).to(contain_metric("Hunger level"))
